282e6d4793bed048fe210d7372d7354c36299c0b
MGC\knagaraj <Kempegowda_Nagaraj@mentor.com>
2020-10-03 10:49:32 +0530

    first commit

---
+++ kafka-producer/src/test/java/com/mi/kafka/producer/KafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mi.kafka.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
